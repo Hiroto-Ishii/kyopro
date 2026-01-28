791256ddd1dc147299b3f5155210e9dcec258974
Hiroto-Ishii <hirototorohiishiishii@gmail.com>
2026-01-29 05:51:34 +0900

    テンプレート編集

--- template.cpp
+++ template.cpp
@@ -1,8 +1,22 @@
+// clang-format off
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace atcoder;
 using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+auto _ = []{ios::sync_with_stdio(false); cin.tie(nullptr); return 0;}();
+const int dy[4] = {-1, 0, 0, 1};
+const int dx[4] = {0, -1, 1, 0};
+const int ddy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+const int ddx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+const int inf = 1 << 30;
+const ll INF = 1LL << 62;
+// clang-format on
 
-int main()
-{
-}
+int main() {}
